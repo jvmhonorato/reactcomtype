f680ef64b65703f18c200e7ee16dbadf0abd9e8a
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-07-27 00:16:53 -0300

    Add formulário no componente Login.tsx
    Add formulário, com states eventos e funções associadas

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,14 +1,39 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+
 
 export const Login = () => {
-    const navigate = useNavigate()
-    const handleClick = () => {
-        navigate('/home')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEnter = () => {
+       // alert(email)
+       // alert(password)
+        console.log(email)
+        console.log(password)
     }
+  
     return(<>
     <div>
-        Login
-        <button onClick={handleClick}>Home</button>
+        <form>
+            <label>
+                <span>Email</span>
+                <input 
+                value={email} 
+                onChange={e => setEmail(e.target.value)}
+                />
+            </label>
+            <label>
+                <span>Senha</span>
+                <input 
+                type="password"
+                 value={password} 
+                onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <button type="button" onClick={handleEnter}>
+                Entrar
+                </button>
+        </form>
+
 
     </div>
     
